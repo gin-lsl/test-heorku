5623f9bf615b5dffedfd8df4e1576643d01c1e40
gin-lsl <gin_lsl@outlook.com>
2017-05-22 20:36:42 +0800

    更新mongo连接

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var cookieParser = require('cookie-parser')
 var expressSession = require('express-session')
 var bodyParser = require('body-parser')
 const MongoStore = require('connect-mongo')(expressSession)
+const mongoConnConfig = require('./configs/mongodb_config').connStr
 var debug = require('debug')('my-app:app')
 
 // 链接数据库
@@ -59,51 +60,51 @@ app.use(cookieParser())
 
 // session
 app.use(expressSession({
-  store: new MongoStore({
-    url: 'mongodb://127.0.0.1:27017/gin_blog'
-  }),
-  secret: credentials.cookieSecret,
-  cookie: session_config.cookie,
-  key: session_config.key,
-  saveUninitialized: true,
-  resave: false
+    store: new MongoStore({
+        url: mongoConnConfig()
+    }),
+    secret: credentials.cookieSecret,
+    cookie: session_config.cookie,
+    key: session_config.key,
+    saveUninitialized: true,
+    resave: false
 }))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
-  debug('session: %O', req.session)
-  next()
+    debug('session: %O', req.session)
+    next()
 })
 
 app.use('/', index)
 
 app.use('/fail', (req, res) => {
-  throw new Error('同步错误')
+    throw new Error('同步错误')
 })
 
 app.use('/async-fail', (req, res) => {
-  process.nextTick(() => {
-    throw new Error('异步错误')
-  })
+    process.nextTick(() => {
+        throw new Error('异步错误')
+    })
 })
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
 })
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
 })
 
 // process.on('uncaughtException', (err) => {
--- configs/mongodb_config.js
+++ configs/mongodb_config.js
@@ -1,11 +1,17 @@
+const connConfig = {
+    development: {
+        connectionString: 'mongodb://127.0.0.1:27017/gin_blog'
+    },
+    production: {
+        connectionString: 'mongodb://' + process.env.dbuser + ':' + process.env.dbpwd + '@ds031257.mlab.com:31257/gin_lsl'
+    }
+}
+
 module.exports = {
     db_name: 'gin_blog',
-    mongoConnConfig: {
-        development: {
-            connectionString: 'mongodb://127.0.0.1:27017/gin_blog'
-        },
-        production: {
-            connectionString: 'mongodb://' + process.env.dbuser + ':' + process.env.dbpwd + '@ds031257.mlab.com:31257/gin_lsl'
-        }
+    mongoConnConfig: connConfig,
+    connStr: function () {
+        let env = process.env.env
+        return connConfig[env].connectionString
     }
 }
